f81320456bca1ff427391fa91a7fd291aadb323d
Prakash2857 <pragashnkl@gmail.com>
2021-09-07 20:24:53 +0530

    Password Functionality

--- src/org/Pages/Login.java
+++ src/org/Pages/Login.java
@@ -2,4 +2,12 @@ package org.Pages;
 
 public class Login {
 
+	public static void password() {
+		System.out.println("password");
+	}
+
+	public static void main(String[] args) {
+		password();
+	}
+
 }
